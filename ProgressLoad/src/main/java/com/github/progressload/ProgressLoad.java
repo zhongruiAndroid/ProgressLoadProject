cc6fdc2d388bdac8797388e6c81eae3f9beb1256
zhongrui <zhongrui@021.com>
2020-07-31 14:46:10 +0800

    file

--- ProgressLoad/src/main/java/com/github/progressload/ProgressLoad.java
+++ ProgressLoad/src/main/java/com/github/progressload/ProgressLoad.java
@@ -1,17 +1,25 @@
 package com.github.progressload;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class ProgressLoad extends View {
     private int pointStartColor;
@@ -23,8 +31,17 @@ public class ProgressLoad extends View {
     private int radius;
     private int pointDirection;
     private int pointStartAngle;
+    private Paint paint;
 
     private Bitmap bitmap;
+    private ArgbEvaluator argbEvaluator;
+    private ValueAnimator valueAnimator;
+    private float drawAngle;
+    public static final int cw=0;
+    public static final int ccw=1;
+    @IntDef({cw,ccw})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Direction{}
 
     public ProgressLoad(Context context) {
         super(context);
@@ -48,16 +65,18 @@ public class ProgressLoad extends View {
     }
 
     private void init(AttributeSet attrs) {
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.FILL);
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.ProgressLoad);
 
-        pointStartColor = typedArray.getColor(R.styleable.ProgressLoad_pointStartColor, Color.parseColor("#19000000"));
+        pointStartColor = typedArray.getColor(R.styleable.ProgressLoad_pointStartColor,Color.parseColor("#29000000"));
         pointEndColor = typedArray.getColor(R.styleable.ProgressLoad_pointEndColor, Color.WHITE);
 
         pointNum = typedArray.getInt(R.styleable.ProgressLoad_pointNum, 12);
-        pointWidth = (int) typedArray.getDimension(R.styleable.ProgressLoad_pointWidth, 6);
-        rotationSpeed = typedArray.getInt(R.styleable.ProgressLoad_rotationSpeed, 3000);
+        pointWidth = (int) typedArray.getDimension(R.styleable.ProgressLoad_pointWidth, 16);
+        rotationSpeed = typedArray.getInt(R.styleable.ProgressLoad_rotationSpeed, 1770);
         rotationDirection = typedArray.getInt(R.styleable.ProgressLoad_rotationDirection, 0);
-        radius = (int) typedArray.getDimension(R.styleable.ProgressLoad_radius, 80);
+        radius = (int) typedArray.getDimension(R.styleable.ProgressLoad_radius, 55);
         pointDirection = typedArray.getInt(R.styleable.ProgressLoad_pointDirection, 0);
         pointStartAngle = typedArray.getInt(R.styleable.ProgressLoad_pointStartAngle, 0);
 
@@ -86,21 +105,164 @@ public class ProgressLoad extends View {
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-
+        super.onSizeChanged(w, h, oldw, oldh);
+        complete();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
+        canvas.rotate((rotationDirection==1?-1:1)*drawAngle,getWidth()/2,getHeight()/2);
+        canvas.drawBitmap(bitmap,getWidth()/2-bitmap.getWidth()/2,getHeight()/2-bitmap.getHeight()/2,null);
     }
     public void complete(){
         int size = radius * 2 + pointWidth * 2;
+        int center=size/2;
         if(bitmap==null||bitmap.getWidth()!=size){
             bitmap=Bitmap.createBitmap(size,size, Bitmap.Config.ARGB_8888);
         }else{
             bitmap.eraseColor(Color.TRANSPARENT);
         }
+        Canvas canvas=new Canvas(bitmap);
+
+        canvas.translate(center,center);
+
+        canvas.rotate(pointStartAngle);
+        if(argbEvaluator==null){
+            argbEvaluator = new ArgbEvaluator();
+        }
+        for (int i = 0; i < pointNum; i++) {
+            int pColor;
+            if(i==pointNum-1){
+                pColor=pointEndColor;
+            }else{
+                pColor= (int) argbEvaluator.evaluate(i*1f/pointNum,pointStartColor,pointEndColor);
+            }
+            paint.setColor(pColor);
+            int tempX = radius + pointWidth / 2;
+            canvas.drawCircle(tempX,0,pointWidth/2,paint);
+
+            canvas.rotate((pointDirection==1?-1:1)*360f/pointNum);
+        }
+
+        startAnim();
     }
+    private void startAnim(){
+        if (valueAnimator == null) {
+            valueAnimator=ValueAnimator.ofFloat(0,360f);
+        }else{
+            valueAnimator.cancel();
+        }
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                drawAngle = (float) animation.getAnimatedValue();
+                if(getVisibility()==VISIBLE){
+                    invalidate();
+                }else if(valueAnimator!=null){
+                    valueAnimator.cancel();
+                }
+            }
+        });
+        valueAnimator.setDuration(rotationSpeed);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.start();
+        invalidate();
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        if(visibility==VISIBLE){
+            startAnim();
+        }else{
+            if(valueAnimator!=null){
+                valueAnimator.cancel();
+            }
+        }
+    }
+
+    public int getPointStartColor() {
+        return pointStartColor;
+    }
+
+    public void setPointStartColor(int pointStartColor) {
+        this.pointStartColor = pointStartColor;
+    }
+
+    public int getPointEndColor() {
+        return pointEndColor;
+    }
+
+    public void setPointEndColor(int pointEndColor) {
+        this.pointEndColor = pointEndColor;
+    }
+
+    public int getPointNum() {
+        return pointNum;
+    }
+
+    public void setPointNum(int pointNum) {
+        if(pointNum<=0){
+            pointNum=1;
+        }else if(pointNum>360){
+            pointNum=360;
+        }
+        this.pointNum = pointNum;
+    }
+
+    public int getPointWidth() {
+        return pointWidth;
+    }
+
+    public void setPointWidth(int pointWidth) {
+        this.pointWidth = pointWidth;
+    }
+
+    public int getRotationSpeed() {
+        return rotationSpeed;
+    }
+
+    public void setRotationSpeed(int rotationSpeed) {
+        if(rotationSpeed<0){
+            rotationSpeed=100;
+        }
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public int getRotationDirection() {
+        return rotationDirection;
+    }
+
+    public void setRotationDirection(@Direction int rotationDirection) {
+        this.rotationDirection = rotationDirection;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getPointDirection() {
+        return pointDirection;
+    }
+
+    public void setPointDirection(@Direction int pointDirection) {
+        this.pointDirection = pointDirection;
+    }
+
+    public int getPointStartAngle() {
+        return pointStartAngle;
+    }
+
+    public void setPointStartAngle(int pointStartAngle) {
+        this.pointStartAngle = pointStartAngle;
+    }
+
+
     private int dp2px(int value) {
         return (int) (getContext().getResources().getDisplayMetrics().density * value);
     }
--- app/src/main/java/com/test/progressloadproject/MainActivity.java
+++ app/src/main/java/com/test/progressloadproject/MainActivity.java
@@ -1,14 +1,169 @@
 package com.test.progressloadproject;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.progressload.ProgressLoad;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
+    private ProgressLoad pl;
+    private AppCompatSeekBar pointNum;
+    private AppCompatSeekBar pointWidth;
+    private AppCompatSeekBar rotationSpeed;
+    private RadioButton rbCw;
+    private RadioButton rbCcw;
+    private AppCompatSeekBar radius;
+    private RadioButton rbPointCw;
+    private RadioButton rbPointCcw;
+    private AppCompatSeekBar pointStartAngle;
+    private Button bt;
+    private Button btVisible;
+    private Button btInVisible;
+    private Button btGone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        pl = findViewById(R.id.pl);
+        pointNum = findViewById(R.id.pointNum);
+        pointNum.setOnSeekBarChangeListener(this);
+        pointNum.setProgress(pl.getPointNum());
+
+        pointWidth = findViewById(R.id.pointWidth);
+        pointWidth.setOnSeekBarChangeListener(this);
+        pointWidth.setProgress(pl.getPointWidth());
+
+        rotationSpeed = findViewById(R.id.rotationSpeed);
+        rotationSpeed.setOnSeekBarChangeListener(this);
+        rotationSpeed.setProgress(pl.getRotationSpeed());
+
+        rbCw = findViewById(R.id.rbCw);
+        rbCw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                pl.setRotationDirection(isChecked?ProgressLoad.cw:ProgressLoad.ccw);
+            }
+        });
+        rbCw.setChecked(pl.getRotationDirection()==ProgressLoad.cw);
+
+        rbCcw = findViewById(R.id.rbCcw);
+        rbCcw.setChecked(pl.getRotationDirection()==ProgressLoad.ccw);
+
+        radius = findViewById(R.id.radius);
+        radius.setOnSeekBarChangeListener(this);
+        radius.setProgress(pl.getRadius());
+
+        rbPointCw = findViewById(R.id.rbPointCw);
+        rbPointCw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                pl.setPointDirection(isChecked?ProgressLoad.cw:ProgressLoad.ccw);
+            }
+        });
+        rbPointCw.setChecked(pl.getPointDirection()==ProgressLoad.cw);
+
+        rbPointCcw = findViewById(R.id.rbPointCcw);
+        rbPointCcw.setChecked(pl.getPointDirection()==ProgressLoad.ccw);
+
+        pointStartAngle = findViewById(R.id.pointStartAngle);
+        pointStartAngle.setOnSeekBarChangeListener(this);
+        pointStartAngle.setProgress(pl.getPointStartAngle());
+
+
+        bt = findViewById(R.id.bt);
+        bt.setOnClickListener(this);
+
+        btVisible = findViewById(R.id.btVisible);
+        btVisible.setOnClickListener(this);
+
+        btInVisible = findViewById(R.id.btInVisible);
+        btInVisible.setOnClickListener(this);
+
+        btGone = findViewById(R.id.btGone);
+        btGone.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+                logStr();
+                pl.complete();
+                break;
+            case R.id.btVisible:
+                pl.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btInVisible:
+                pl.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.btGone:
+                pl.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+    private void logStr() {
+        int pointNum = pl.getPointNum();
+        int pointWidth = pl.getPointWidth();
+        int rotationSpeed = pl.getRotationSpeed();
+        int rotationDirection = pl.getRotationDirection();
+        int radius = pl.getRadius();
+        int pointDirection = pl.getPointDirection();
+        int pointStartAngle = pl.getPointStartAngle();
+        Log.i("=====","===================================");
+        Log.i("=====","pointNum====="+pointNum);
+        Log.i("=====","pointWidth====="+pointWidth);
+        Log.i("=====","rotationSpeed====="+rotationSpeed);
+        Log.i("=====","rotationDirection====="+rotationDirection);
+        Log.i("=====","radius====="+radius);
+        Log.i("=====","pointDirection====="+pointDirection);
+        Log.i("=====","pointStartAngle====="+pointStartAngle);
+        Log.i("=====","===================================");
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.pointNum:
+                pl.setPointNum(progress);
+                break;
+            case R.id.pointWidth:
+                pl.setPointWidth(progress);
+                break;
+            case R.id.rotationSpeed:
+                pl.setRotationSpeed(progress);
+                break;
+            case R.id.radius:
+                pl.setRadius(progress);
+                break;
+            case R.id.pointStartAngle:
+                pl.setPointStartAngle(progress);
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
